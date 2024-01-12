88f7b3b681baeec9fce3a6ab8472c8831944126d
stevennoad <110924473+stevennoad@users.noreply.github.com>
2024-01-12 15:20:11 +0000

    Added caching to reduce costs on repart page loads

--- geo.js
+++ geo.js
@@ -1,26 +1,46 @@
 <script>
 	window.addEventListener('DOMContentLoaded', function () {
-		geoip2.country(
-			function (geoipResponse) {
-				var isoCode = geoipResponse.country.iso_code;
-				var ukBoxes = document.querySelectorAll('.uk');
-				var euroBoxes = document.querySelectorAll('.euro');
+		const storedData = JSON.parse(localStorage.getItem('geo-hide'));
 
-				ukBoxes.forEach(function (uk, index) {
-					var euro = euroBoxes[index];
+		if (storedData && isNotExpired(storedData.timestamp)) {
+			showHideClasses(storedData.isoCode);
+		} else {
+			geoip2.country(
+				function (geoipResponse) {
+					const isoCode = geoipResponse.country.iso_code;
+					const dataToStore = {
+						isoCode: isoCode,
+						timestamp: Date.now()
+					};
 
-					if (isoCode === 'GB') {
-						uk.classList.remove('hidden');
-						euro.classList.add('hidden');
-					} else {
-						uk.classList.add('hidden');
-						euro.classList.remove('hidden');
-					}
-				});
-			},
-			function (error) {
-				console.log('An error occurred. Please try again.');
-			}
-		);
+					localStorage.setItem('geo-hide', JSON.stringify(dataToStore));
+					showHideClasses(isoCode);
+				},
+				function (error) {
+					console.log('An error occurred. Please try again.');
+				}
+			);
+		}
 	});
+
+	function isNotExpired(timestamp) {
+		return Date.now() - timestamp < 30 * 24 * 60 * 60 * 1000;
+	}
+
+	function showHideClasses(isoCode) {
+		const isUk = document.querySelectorAll('.uk');
+		const isEuro = document.querySelectorAll('.euro');
+
+		isUk.forEach(function (uk, index) {
+			const euro = isEuro[index];
+
+			if (isoCode === 'GB') {
+				uk.classList.remove('hidden');
+				euro.classList.add('hidden');
+			} else {
+				uk.classList.add('hidden');
+				euro.classList.remove('hidden');
+			}
+		});
+	}
 </script>
